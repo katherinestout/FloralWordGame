f57ba7e56523683337582db80a3c918678c77845
katherinestout <kjs14@hood.edu>
2018-11-06 22:27:04 -0500

    start

---
+++ assets/javascript.js
@@ -0,0 +1,18 @@
+
+var wordBank = ['republican', 'democrat', 'trump', 'whitehouse', 'DC', 'vote'];
+
+var wins = 0;
+var losses = 0;
+var currentWord = '';
+var guessesLeft = 10;
+
+var guessedLetters = [];
+var incorrectLetters = [];
+
+
+
+$(document).ready(function(){
+
+    
+
+});
