520da6911271b12f2676517a815dc0e66629debd
katherinestout <kjs14@hood.edu>
2018-11-08 16:19:02 -0500

    sortin out some stuff

--- assets/javascript.js
+++ assets/javascript.js
@@ -4,6 +4,9 @@ var wordBank = ['republican', 'democrat', 'trump', 'whitehouse', 'DC', 'vote'];
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
                     'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; 
 //variables
+//for each item in array, attach an id or value
+
+
 var wins = 0;
 var losses = 0;
 
@@ -20,26 +23,33 @@ var i;
   for (var i = 0; i < alphabet.length; i++){
       //console.log(alphabet[i]);
      //onclick = console.log("hello");
-
-      alphabet.id = alphabet[i];
-      
-      
+      id = alphabet[i];
+   // console.log(id);
       text += "<button>" + alphabet[i] + "</button>";
     //console.log(alphabet.id);
   // console.log(alphabet[20]);
-   
-      
-}
 
+  check();
+}
 //alphabet.forEach(id => console.log(id));
 //console.log(text);
 document.getElementById("alphabet").innerHTML = text;
 //console.log(text);
 
+/*
 $("#incorrectGuesses").click(function(){
   console.log("this is a click example");
-});
+}); */
 
+ function check(){
+  $("#alphabet").click(function(){
+    alphabet.forEach(value => console.log(value));
+  });
+}
+console.log(Object.values(alphabet));
+console.log(Object.values(alphabet[20]));
+
+//console log each value of each item in array
 
 //Generate a random word from the wordBank, store it in randomWord  
 var currentWord = wordBank[Math.floor(Math.random() * wordBank.length)];
