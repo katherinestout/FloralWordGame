23fbbf6285124ece18714182ab2a1a13049d60c1
katherinestout <kjs14@hood.edu>
2018-11-09 11:36:48 -0500

    gettin there

--- assets/javascript.js
+++ assets/javascript.js
@@ -15,43 +15,45 @@ var guessesLeft = 10;
 var guessedLetters = [];
 var incorrectLetters = [];
 var space;
+/*
+$.each(alphabet, function(key, value){
+  $('#' + key + '<button>').click(function(){
+    $('.'+key).append(value);
+  });
+});*/
 
 //for each letter in the alphabet, list it out and create a button
+
 var text ="";
 var i;
 
   for (var i = 0; i < alphabet.length; i++){
-      //console.log(alphabet[i]);
-     //onclick = console.log("hello");
-   // id = alphabet[i];
-   // console.log(id);
       text += "<button>" + alphabet[i] + "</button>";
-    //console.log(alphabet.id);
   // console.log(alphabet[20]);
-
+  console.log(i );
   check();
 }
+
+
 //alphabet.forEach(id => console.log(id));
 //console.log(text);
 document.getElementById("alphabet").innerHTML = text;
 //console.log(text);
 
-/*
-$("#incorrectGuesses").click(function(){
-  console.log("this is a click example");
-}); */
 
  function check(){
-  $("#alphabet").click(function(){
-    alphabet.forEach(value => console.log(value));
-    console.log(Object.values(alphabet));
+  $("body").on("click", "#alphabet button", function(){
+   // alphabet.forEach(value => console.log(value));
+    //console.log(Object.values(alphabet));
+    console.log("help");
+
   });
 }
 
 //when you click on each button console log the value of the letter
 
-console.log(Object.values(alphabet));
-console.log(Object.values(alphabet[20]));
+  //console.log(Object.values(alphabet));
+  console.log(Object.values(alphabet[20]));
 
 //console log each value of each item in array
 
