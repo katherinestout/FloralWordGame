3c25e2accb142bb7ba2f621818917b3f4648904f
katherinestout <kjs14@hood.edu>
2018-11-13 15:02:20 -0500

    values

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,11 @@ window.onload = function(){
 var wordBank = ['republican', 'democrat', 'trump', 'whitehouse', 'DC', 'vote'];
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
                     'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; 
+
+                    var iterator = alphabet.values();
+                    for (const value of iterator){
+                      console.log(value);
+                    }
 //variables
 //for each item in array, attach an id or value
 
@@ -17,59 +22,57 @@ var guessedLetters = [];
 var incorrectLetters = [];
 var space;
 
-//for each letter in the alphabet, list it out and create a button
-var text ="";
-var i;
-for (var i = 0; i < alphabet.length; i++){
-    var letter = alphabet[i];
-     text += "<button>" + letter + "</button>";
 
 
-  check();
-}
-//alphabet.forEach(value => console.log(value));
-document.getElementById("alphabet").innerHTML = text;
-//alphabet.forEach(id => console.log(id));
-
- function check(){
-  $("body").on("click", "#alphabet button", function(){
-   // $("body").on("click", "#list ul li", function(){
-  // alphabet.forEach(value => console.log(value));
-    //console.log(this.innerHTML);
-    var guess = this.innerHTML;
-    console.log(guess);
-    //if guess letter is in any of the word, consolelog yaya
-  });
-}
-
-
+/*
 let add_btns = document.querySelector('.btn');
 add_btns.addEventListener("click", function(){
 
   let btn = document.createElement("BUTTON");
   alphabet.push(document.body.appendChild(btn));
-  i++;
+ 
   for (let button in alphabet){
     btn.setAttribute('id', 'button' + button);
     btn.innerHTML = "Button #" + button;
   
     btn.addEventListener("click", function () {
       let id = this.id;
-      alert("my id is: " + id);
+      console.log("my id is: " + id);
   });
   }
 })
-
+*/
 
 function start(){
   $("body").on("click", "#start", function(){
 //Generate a random word from the wordBank, store it in randomWord  
 var currentWord = wordBank[Math.floor(Math.random() * wordBank.length)];
     console.log(currentWord);
-    
 
+   //for each letter in the alphabet, list it out and create a button
+var text ="";
+var i;
+for (var i = 0; i < alphabet.length; i++){
+    var letter = alphabet[i];
+     text += "<button>" + letter + "</button>";
+  check();
+}
+//alphabet.forEach(value => console.log(value));
+document.getElementById("alphabet").innerHTML = text;
+//alphabet.forEach(id => console.log(id));
+  });
+}
+
+
+function check(){
+  $("body").on("click", "#alphabet button", function(){
+   // $("body").on("click", "#list ul li", function(){
+  // alphabet.forEach(value => console.log(value));
+    //console.log(this.innerHTML);
   
-
+    var guess = this.innerHTML;
+    console.log(guess);
+    //if guess letter is in any of the word, consolelog yaya
   });
 }
 start();
@@ -77,13 +80,5 @@ start();
 
   //console.log(Object.values(alphabet));
  // console.log(Object.values(alphabet[20]));
-
-//console log each value of each item in array
-
-
-//if the letter they type in the space does not match the current word, put the letter in incorrect lettes
-//if the letter they type in does match one of the current word, put it in guessed letters
-//each time they type in a letter the guessesLeft goes down.
-
 
 }
