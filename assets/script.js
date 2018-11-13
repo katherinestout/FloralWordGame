2411639d7444b5c9927fcb7bc24166bf70419dc8
katherinestout <kjs14@hood.edu>
2018-11-13 15:46:35 -0500

    innerhtml

--- assets/script.js
+++ assets/script.js
@@ -4,10 +4,7 @@ var wordBank = ['republican', 'democrat', 'trump', 'whitehouse', 'DC', 'vote'];
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
                     'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; 
 
-                    var iterator = alphabet.values();
-                    for (const value of iterator){
-                      console.log(value);
-                    }
+                   
 //variables
 //for each item in array, attach an id or value
 
@@ -43,42 +40,40 @@ add_btns.addEventListener("click", function(){
 })
 */
 
+
 function start(){
   $("body").on("click", "#start", function(){
+
 //Generate a random word from the wordBank, store it in randomWord  
 var currentWord = wordBank[Math.floor(Math.random() * wordBank.length)];
     console.log(currentWord);
 
    //for each letter in the alphabet, list it out and create a button
 var text ="";
-var i;
-for (var i = 0; i < alphabet.length; i++){
-    var letter = alphabet[i];
-     text += "<button>" + letter + "</button>";
-  check();
+
+var iterator = alphabet.values();
+for (const value of iterator){
+      var btnval = value;
+     text += "<button>" + btnval + "</button>";
+
 }
+btnval.onclick = check();
+  function check(){
+    $("body").on("click", "#alphabet button", function(){
+      //console.log(this.innerHTML);
+      //everytie you click a button you get the value of that letter
+        console.log(this);
+      //if guess letter is in any of the word, consolelog yaya
+    });
+  }
+
 //alphabet.forEach(value => console.log(value));
 document.getElementById("alphabet").innerHTML = text;
 //alphabet.forEach(id => console.log(id));
   });
 }
-
-
-function check(){
-  $("body").on("click", "#alphabet button", function(){
-   // $("body").on("click", "#list ul li", function(){
-  // alphabet.forEach(value => console.log(value));
-    //console.log(this.innerHTML);
-  
-    var guess = this.innerHTML;
-    console.log(guess);
-    //if guess letter is in any of the word, consolelog yaya
-  });
-}
 start();
 //when you click on each button console log the value of the letter
-
   //console.log(Object.values(alphabet));
- // console.log(Object.values(alphabet[20]));
-
+  //console.log(Object.values(alphabet[20]));
 }
